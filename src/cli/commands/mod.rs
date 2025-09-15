a937ca5f6fd616f33bc4e5e71d797f03cfc703da
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-09-15 13:39:37 +0200

    Cargo fmt.

--- src/cli/commands/mod.rs
+++ src/cli/commands/mod.rs
@@ -35,11 +35,9 @@ pub enum Command {
     // Signer(self::signer::Signer),
     // - Command: add/remove/modify a zone // TODO: ask Arya what we meant by that
     // - Command: resign a zone immediately (optionally with custom config)
-
     /// Manage HSMs
     #[command(name = "hsm")]
     Hsm(self::hsm::Hsm),
-
     // /// Show the manual pages
     // Help(self::help::Help),
 }
@@ -47,10 +45,22 @@ pub enum Command {
 impl Command {
     pub async fn execute(self, client: CascadeApiClient) -> Result<(), String> {
         match self {
-            Self::Zone(zone) => zone.execute(client).await.map_err(|err| format!("zone command failed: {err}")),
-            Self::Status(status) => status.execute(client).await.map_err(|err| format!("status command failed: {err}")),
-            Self::Policy(policy) => policy.execute(client).await.map_err(|err| format!("policy command failed: {err}")),
-            Self::Hsm(hsm) => hsm.execute(client).await.map_err(|err| format!("hsm command failed: {err}")),
+            Self::Zone(zone) => zone
+                .execute(client)
+                .await
+                .map_err(|err| format!("zone command failed: {err}")),
+            Self::Status(status) => status
+                .execute(client)
+                .await
+                .map_err(|err| format!("status command failed: {err}")),
+            Self::Policy(policy) => policy
+                .execute(client)
+                .await
+                .map_err(|err| format!("policy command failed: {err}")),
+            Self::Hsm(hsm) => hsm
+                .execute(client)
+                .await
+                .map_err(|err| format!("hsm command failed: {err}")),
             // Self::Help(help) => help.execute(),
         }
     }
