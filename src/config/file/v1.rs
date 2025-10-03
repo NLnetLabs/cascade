9f0cca8d26984b5dc8657b5875e9db19c0bf8622
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-10-03 18:13:37 +0200

    Update src/config/file/v1.rs
    Co-authored-by: Jannik Peters <jannik@nlnetlabs.nl>

--- src/config/file/v1.rs
+++ src/config/file/v1.rs
@@ -124,7 +124,7 @@ impl Spec {
 
     /// The default value for `dnst_binary_path`.
     fn dnst_binary_path_default() -> Box<Utf8Path> {
-        "/var/lib/cascade/bin/cascade-dnst".into()
+        "/usr/libexec/cascade/cascade-dnst".into()
     }
 
     /// The default value for `dnst_keyset_dir`.
