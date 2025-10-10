bd0947ee60b4c935412fda1368e47cadee154ad1
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-10-10 14:14:51 +0200

    Extend post-packaging tests to try and start the systemd unit (#200)

--- pkg/test-scripts/test-cascade.sh
+++ pkg/test-scripts/test-cascade.sh
@@ -25,5 +25,23 @@ case $1 in
 
     echo -e "\nCASCADED MAN PAGE (first 20 lines only):"
     man -P cat cascaded | head -n 20 || true
+
+    echo -e "\nCASCADED SERVICE SHOULD BE STOPPED BY DEFAULT:"
+    if systemctl is-active cascaded; then
+      echo "Systemd 'cascaded' service is unexpectedly active"
+      exit 1
+    fi
+
+    echo -e "\nTEST SYSTEMD SERVICE STARTUP:"
+    systemctl start cascaded
+
+    # Give it time to start
+    sleep 3s
+
+    # Dump the status
+    systemctl status cascaded
+
+    # Check that the service is active
+    systemctl is-active cascaded
     ;;
 esac
