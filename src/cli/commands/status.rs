856134b3e8065263073835b21a16f80bcc07b7bd
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-10-15 00:11:58 +0200

    Colour and layout tweaks.

--- src/cli/commands/status.rs
+++ src/cli/commands/status.rs
@@ -37,18 +37,19 @@ impl Status {
                     .map_err(format_http_error)?;
 
                 if !response.hard_halted_zones.is_empty() {
-                    eprintln!("The following zones are hard-halted due to a serious problem:");
+                    eprintln!("The following zones are halted due to a serious problem:");
                     for (zone_name, err) in response.hard_halted_zones {
-                        eprintln!("  - {zone_name}: {err}");
+                        eprintln!("   {}\u{78}{} {zone_name}: {err}", ansi::RED, ansi::RESET);
                     }
                     eprintln!();
                 }
 
                 if !response.soft_halted_zones.is_empty() {
-                    eprintln!("The following zones are soft-halted:");
+                    eprintln!("The following zones are halted due to resolvable issues:");
                     for (zone_name, err) in response.soft_halted_zones {
-                        eprintln!("  - {zone_name}: {err}");
+                        eprintln!("   {}\u{78}{} {zone_name}: {err}", ansi::RED, ansi::RESET);
                     }
+                    eprintln!();
                 }
 
                 println!("Signing queue:");
@@ -56,12 +57,12 @@ impl Status {
                     println!("  The signing queue is currently empty.");
                 } else {
                     println!(
-                        "  Key: {}In Progress (>){}, {}Pending (-){}, {}Finished (<){}",
-                        ansi::GREEN,
-                        ansi::RESET,
-                        ansi::CYAN,
+                        "  Key: {}In Progress (\u{23F5}){}, {}Pending (\u{23F8}){}, {}Finished (\u{2714}){}",
+                        ansi::YELLOW,
                         ansi::RESET,
                         ansi::GRAY,
+                        ansi::RESET,
+                        ansi::GREEN,
                         ansi::RESET
                     );
                     println!("  {:>2}:   {:<25} {:<16} Action", "#", "When", "Zone");
@@ -69,14 +70,14 @@ impl Status {
                         let zone_name = report.zone_name.to_string();
                         let action = &report.signing_report.current_action;
                         let (colour, state, when) = match &report.signing_report.stage_report {
-                            SigningStageReport::Requested(r) => (ansi::CYAN, "-", r.requested_at),
-                            SigningStageReport::InProgress(r) => (ansi::GREEN, ">", r.started_at),
-                            SigningStageReport::Finished(r) => (ansi::GRAY, "<", r.finished_at),
+                            SigningStageReport::Requested(r) => (ansi::GRAY, "\u{23F8}", r.requested_at),
+                            SigningStageReport::InProgress(r) => (ansi::YELLOW, "\u{23F5}", r.started_at),
+                            SigningStageReport::Finished(r) => (ansi::GREEN, "\u{2714}", r.finished_at),
                         };
                         let when = DateTime::<Utc>::from(when)
                             .to_rfc3339_opts(chrono::SecondsFormat::Secs, false);
                         println!(
-                            "{colour}  {i:>2}: {state} {when:<25} {zone_name:<16} {action}{}",
+                            "  {i:>2}: {colour}{state}{} {when:<25} {zone_name:<16} {action}",
                             ansi::RESET
                         );
                     }
