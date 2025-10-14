38e7252cc959ffe1149f3c6a6c35d7871ed9178a
Jannik Peters <jannik@nlnetlabs.nl>
2025-10-14 20:37:13 +0200

    Update docs to include SERVER_IP and _PORT

--- approve_or_deny_signed.sh
+++ approve_or_deny_signed.sh
@@ -3,13 +3,11 @@
 # - CASCADE_ZONE
 # - CASCADE_SERIAL
 # - CASCADE_SERVER
+# - CASCADE_SERVER_IP
+# - CASCADE_SERVER_PORT
 
 set -euo pipefail -x
 
 echo "Hook invoked with $*"
 
-SERVER_IP=${CASCADE_SERVER%:*}
-SERVER_IP_DIG="${SERVER_IP//[\[\]]/}" # remove brackets from IPv6
-SERVER_PORT=${CASCADE_SERVER##*:} # Using double '##' in case its an IPv6
-
-dig +noall +onesoa +answer "@$SERVER_IP_DIG" -p "$SERVER_PORT" "${CASCADE_ZONE}" AXFR | dnssec-verify -o "${CASCADE_ZONE}" /dev/stdin /tmp/keys/
+dig +noall +onesoa +answer "@$CASCADE_SERVER_IP" -p "$CASCADE_SERVER_PORT" "${CASCADE_ZONE}" AXFR | dnssec-verify -o "${CASCADE_ZONE}" /dev/stdin /tmp/keys/
