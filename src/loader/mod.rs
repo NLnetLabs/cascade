78a557d840f9bd06f6b968653d27c85fd4693f04
Terts Diepraam <terts.diepraam@gmail.com>
2025-12-10 14:52:57 +0100

    add a happy little zonefile test

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -1,4 +1,5 @@
-//! Loading zones.//!
+//! Loading zones.
+//!
 //! The zone loader is responsible for maintaining up-to-date copies of the DNS
 //! zones known to Nameshed.  Every zone has a configured source (e.g. zonefile,
 //! DNS server, etc.) that will be monitored for changes.
