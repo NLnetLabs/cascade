91192d4f2d3f9e535fb1d22d92bbaeaa4d644345
Jannik Peters <jannik@nlnetlabs.nl>
2026-02-19 16:12:04 +0100

    Clippy

--- build.rs
+++ build.rs
@@ -3,6 +3,8 @@
 // `println!`s of this file. Resolve all warning first, trigger a re-build
 // (e.g. `touch build.rs`), and run `cargo -vv build` again.
 
+#![allow(clippy::disallowed_macros, reason = "we're not printing in color")]
+
 use std::ffi::OsStr;
 use std::path::PathBuf;
 use std::process::{Command, Output};
@@ -34,20 +36,21 @@ fn main() {
     let package = env!("CARGO_PKG_NAME");
     let is_jj = match package {
         "cascaded" => PathBuf::from(".jj").exists(),
-        "cascade" | _ => PathBuf::from("../../.jj").exists(),
+        "cascade" => PathBuf::from("../../.jj").exists(),
         // This build script is only run for the packages `cascade` and
         // `cascaded`. If we link the build.rs file to other packages too, we
-        // expect them to be in `crates/`.
+        // need to add them to the match arm above.
+        _ => unreachable!(),
     };
 
     let git_root = if is_jj {
-        run_cmd_strip("jj", &["workspace", "root", "--ignore-working-copy"])
+        run_cmd_strip("jj", ["workspace", "root", "--ignore-working-copy"])
     } else {
-        let is_in_git_worktree = run_cmd("git", &["rev-parse", "--is-inside-work-tree"])
+        let is_in_git_worktree = run_cmd("git", ["rev-parse", "--is-inside-work-tree"])
             .status
             .success();
         if is_in_git_worktree {
-            run_cmd_strip("git", &["rev-parse", "--show-toplevel"])
+            run_cmd_strip("git", ["rev-parse", "--show-toplevel"])
         } else {
             match package {
                 "cascaded" => println!("cargo::rerun-if-changed=.git"),
@@ -79,14 +82,14 @@ fn main() {
     if is_jj {
         generate_project_rerun_with_prefix(&git_root, vec![".jj/working_copy/checkout"]);
     } else {
-        let git_head_ref_cmd = run_cmd("git", &["symbolic-ref", "HEAD"]);
-        let git_dir = run_cmd_strip("git", &["rev-parse", "--git-dir"]);
+        let git_head_ref_cmd = run_cmd("git", ["symbolic-ref", "HEAD"]);
+        let git_dir = run_cmd_strip("git", ["rev-parse", "--git-dir"]);
 
         generate_project_rerun_with_prefix(&git_dir, vec!["HEAD"]);
 
         if git_head_ref_cmd.status.success() {
             let git_head_ref = strip_newline(String::from_utf8(git_head_ref_cmd.stdout).unwrap());
-            let git_common_dir = run_cmd_strip("git", &["rev-parse", "--git-common-dir"]);
+            let git_common_dir = run_cmd_strip("git", ["rev-parse", "--git-common-dir"]);
             generate_project_rerun_with_prefix(&git_common_dir, vec![&git_head_ref]);
         } else {
             // .git/HEAD is likely a detached HEAD right now (aka contains
@@ -103,7 +106,7 @@ fn generate_version_string(is_jj: bool) -> String {
     let (mut git_hash, is_dirty) = if is_jj {
         let git_hash = run_cmd_strip(
             "jj",
-            &[
+            [
                 "log",
                 "--ignore-working-copy",
                 "-G",
@@ -113,15 +116,15 @@ fn generate_version_string(is_jj: bool) -> String {
                 "@-",
             ],
         );
-        let is_dirty = match run_cmd_strip("jj", &["log", "-G", "-T", "empty"]).as_str() {
+        let is_dirty = match run_cmd_strip("jj", ["log", "-G", "-T", "empty"]).as_str() {
             "true" => false,
             "false" => true,
             _ => unreachable!(),
         };
         (git_hash, is_dirty)
     } else {
-        let git_hash = run_cmd_strip("git", &["rev-parse", "--short", "HEAD"]);
-        let is_dirty = !run_cmd("git", &["diff-index", "--quiet", "HEAD"])
+        let git_hash = run_cmd_strip("git", ["rev-parse", "--short", "HEAD"]);
+        let is_dirty = !run_cmd("git", ["diff-index", "--quiet", "HEAD"])
             .status
             .success();
         (git_hash, is_dirty)
