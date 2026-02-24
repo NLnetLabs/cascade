dec576042f71945cfbfa07c71b89cd5bf7cb326f
Philip Homburg <philip@nlnetlabs.nl>
2026-02-24 14:30:05 +0100

    More tests and ZONEMD.

--- integration-tests/incremental-signing/scripts/gen-reference-output.sh
+++ integration-tests/incremental-signing/scripts/gen-reference-output.sh
@@ -1,23 +1,39 @@
 #!/bin/sh
 INCEPTION=1600000000
 EXPIRATION=1700000000
-for m in nsec nsec3 nsec3-opt-out
+for zonemd in '' 'zmd384'
 do
-	case "$m" in
-	nsec)
-		params=""
+	case "$zonemd" in
+	'')
+		zonemd_params=""
+		zonemd_output_name=""
 	;;
-	nsec3)
-		params="-n"
-	;;
-	nsec3-opt-out)
-		params="-n -P"
+	zmd384)
+		zonemd_params="-z SHA384"
+		zonemd_output_name=".zmd384"
 	;;
 	esac
-	for z in zones/*input[23].zone
+	for m in nsec nsec3 nsec3-opt-out
 	do
-		echo $z
-		dnst signzone -T -o example -f - -e $EXPIRATION -i $INCEPTION $params $z keys/Kexample.+015+02835 |
-			sort -u > reference-output/$(basename $z).$m.signed.sorted
+		case "$m" in
+		nsec)
+			params=""
+		;;
+		nsec3)
+			params="-n"
+		;;
+		nsec3-opt-out)
+			params="-n -P"
+		;;
+		esac
+		for z in zones/*input[23].zone
+		do
+			echo $z
+			echo $zonemd $zonemd_params $zonemd_output_name
+			# Remove ZONEMD from input until the bug in sigzone is fixed.
+			ldns-zcat -o example $z | ldns-read-zone -e ZONEMD > /tmp/input
+			dnst signzone -T -o example -f - -e $EXPIRATION -i $INCEPTION $params $zonemd_params /tmp/input keys/Kexample.+015+02835 |
+				sort -u > reference-output/$(basename $z).${m}${zonemd_output_name}.signed.sorted
+		done
 	done
 done
