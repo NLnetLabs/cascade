672daf045e5bb7234935bf2410f36b3bdc00ee68
Jannik Peters <jannik@nlnetlabs.nl>
2026-02-27 11:22:13 +0100

    Document build.rs use and function

--- build.rs
+++ build.rs
@@ -1,3 +1,30 @@
+//! This build script generates the version string for cascade and cascaded.
+//!
+//! It tries to determine which git commit is used to build the binary
+//! currently being built, and adds that commit hash to the version string for
+//! reference in issues. This can be disabled with the
+//! CASCADE_SKIP_VERSION_COMMIT environment variable set to 1 or true.
+//!
+//! As some of our developers use jj as a wrapper around git, the usual
+//! assumptions that the repository always contains a .git file or directory,
+//! does not hold. Therefore, we check for the .jj directory and use the jj
+//! tool if that directory exists. Otherwise we use the normal git approach.
+//!
+//! By default, Cargo will re-run the build script if any of the files in the
+//! package change. However, we also need to re-run the build script when the
+//! git state changes. For example, when someone built the package in a dirty
+//! worktree to test their changes (which results in the version string to
+//! include <commit>-dirty) and then commits the changes to git, no files in
+//! the package have changed but the version string needs to be updated to the
+//! freshly created commit. Therefore, we instruct Cargo with the rerun-if
+//! statements to re-run the build script when any of the files tracking
+//! commit state have changed. However, as using rerun-if disables the
+//! default behaviour, we also need to generate the rerun-if statements for
+//! the relevant source files of the package [1], [2].
+//!
+//! [1]: https://doc.rust-lang.org/cargo/reference/build-scripts.html#change-detection
+//! [2]: https://doc.rust-lang.org/cargo/reference/build-scripts.html#rerun-if-changed
+
 // Note to developers extending/debugging this file: When this file throws
 // errors or warnings, `cargo -vv build` does not show the output of the
 // `println!`s of this file. Resolve all warning first, trigger a re-build
@@ -33,6 +60,7 @@ where
 }
 
 fn main() -> Result<(), ()> {
+    // Allow disabling including the git commit, e.g. for release builds.
     if matches!(
         option_env!("CASCADE_SKIP_VERSION_COMMIT"),
         Some("true" | "1")
@@ -44,6 +72,7 @@ fn main() -> Result<(), ()> {
     // Rust build scripts are per package, therefore this script is both run
     // from the project's root directory and the crates sub-directory.
     let package = env!("CARGO_PKG_NAME");
+    // Check whether this repository is managed by jj or not.
     let is_jj = match package {
         "cascaded" => PathBuf::from(".jj").exists(),
         "cascade" => PathBuf::from("../../.jj").exists(),
@@ -53,7 +82,16 @@ fn main() -> Result<(), ()> {
         _ => unreachable!(),
     };
 
+    // Determine the repository's root directory, if it is in a repository and
+    // not just a directory from a downloaded tar-ball of a release.
     let git_root = if is_jj {
+        // While the existance of a .jj directory doesn't necessarily mean
+        // that we cannot fall-back to using git, we won't fall-back to git to
+        // avoid potential false commit hashes. A jj worktree located inside
+        // an existing git repository does not contain a .git file or
+        // directory. Therefore, git will act on the git repository containing
+        // the worktre and report wrong status and checked out commits.
+
         // On first run of jj, check if it executes ok (aka do not unwrap, but catch the error)
         run_cmd_strip("jj", ["workspace", "root", "--ignore-working-copy"]).map_err(|e| {
             let msg = match e.kind() {
@@ -64,6 +102,9 @@ fn main() -> Result<(), ()> {
             println!(
                 "cargo::warning=A .jj directory exists, but {msg}. Unable to determine git revision..."
             );
+
+            // jj failed, so we instruct Cargo to re-run the build script when
+            // the jj state changes.
             match package {
                 "cascaded" => println!("cargo::rerun-if-changed=.jj"),
                 "cascade" => println!("cargo::rerun-if-changed=../../.jj"),
@@ -80,6 +121,9 @@ fn main() -> Result<(), ()> {
                     _ => "unknown error occured while attempting to run git",
                 };
                 println!("cargo::warning={msg}. Unable to determine git revision...");
+
+                // git failed to execute, instruct Cargo to re-run the build
+                // script when the git state changes.
                 match package {
                     "cascaded" => println!("cargo::rerun-if-changed=.git"),
                     "cascade" => println!("cargo::rerun-if-changed=../../.git"),
@@ -118,6 +162,7 @@ fn main() -> Result<(), ()> {
         if is_jj {
             generate_project_rerun_with_prefix(&git_root, vec![".jj/working_copy/checkout"]);
         } else {
+            // Check which branch HEAD points to (if any).
             let git_head_ref_cmd = run_cmd("git", ["symbolic-ref", "HEAD"]).unwrap();
             let git_dir = run_cmd_strip("git", ["rev-parse", "--git-dir"]).unwrap();
 
@@ -128,6 +173,7 @@ fn main() -> Result<(), ()> {
                     strip_newline(String::from_utf8(git_head_ref_cmd.stdout).unwrap());
                 let git_common_dir =
                     run_cmd_strip("git", ["rev-parse", "--git-common-dir"]).unwrap();
+                // Monitor the branch's "HEAD", which contains the current commit
                 generate_project_rerun_with_prefix(&git_common_dir, vec![&git_head_ref]);
             } else {
                 // .git/HEAD is likely a detached HEAD right now (aka contains
