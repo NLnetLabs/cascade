0cb1bc27cb577baaeb7648be2a6f3c8b5d426f94
Jannik Peters <jannik@nlnetlabs.nl>
2026-03-02 13:17:26 +0100

    Mention remote control and metrics in log message

--- src/manager.rs
+++ src/manager.rs
@@ -94,7 +94,7 @@ impl Manager {
             // Unwrap, because there should always be a valid IPv4/IPv6
             // address. Otherwise this socket couldn't have been created.
             let addr = socket.local_addr().unwrap();
-            info!("Obtained TCP listener for HTTP Server on address {addr}");
+            info!("Obtained TCP listener for HTTP server for remote-control and metrics on address {addr}");
         }
 
         info!("Starting unit 'PS'");
