c68bd7c72946931049124ec8d193ee98fe65851c
arya dradjica <arya@nlnetlabs.nl>
2026-04-10 10:12:19 +0200

    [zonedata/transitions] Omit the assertion entirely

--- crates/zonedata/src/storage/transitions.rs
+++ crates/zonedata/src/storage/transitions.rs
@@ -443,12 +443,7 @@ impl CleanLoadedPendingStorage {
             Arc::ptr_eq(old_reviewer.data(), &self.data),
             "'old_reviewer' is for a different zone"
         );
-        // If a diff existed, then '!curr_loaded_index' must be used.
-        assert!(
-            old_reviewer.loaded_diff().is_some()
-                == (old_reviewer.loaded_index != self.curr_loaded_index),
-            "'old_reviewer' does not point to the instance being cleaned",
-        );
+        // TODO: Verify that 'old_reviewer' is the right reviewer.
 
         // NOTE: We clean '!curr_loaded_index', *even if that was not the one
         // being reviewed*. 'curr_loaded_index' is in use and thus must not be
