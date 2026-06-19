8a770b86790cb8e0dc7352849865e1d57e9b35e1
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2026-06-19 14:10:07 +0200

    Cargo fmt.

--- src/persistence/zone.rs
+++ src/persistence/zone.rs
@@ -863,17 +863,19 @@ impl IxfrZoneDiffs {
 
         if let Some(loaded_serial) = loaded_serial
             && let Some(e) = self.loaded_diffs.last_entry()
-                && *e.key() == u32::from(loaded_serial) {
-                    trace!("Discarding loaded diff for serial {loaded_serial}");
-                    self.loaded_diffs.pop_last();
-                }
+            && *e.key() == u32::from(loaded_serial)
+        {
+            trace!("Discarding loaded diff for serial {loaded_serial}");
+            self.loaded_diffs.pop_last();
+        }
 
         if let Some(signed_serial) = signed_serial
             && let Some(e) = self.signed_diffs.last_entry()
-                && *e.key() == u32::from(signed_serial) {
-                    trace!("Discarding signed diff for serial {signed_serial}");
-                    self.signed_diffs.pop_last();
-                }
+            && *e.key() == u32::from(signed_serial)
+        {
+            trace!("Discarding signed diff for serial {signed_serial}");
+            self.signed_diffs.pop_last();
+        }
     }
 }
 
