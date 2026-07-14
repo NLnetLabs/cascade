c0a367898c0f5edf2b24edbabb27f831e6ea7753
Terts Diepraam <terts.diepraam@gmail.com>
2026-07-14 15:10:16 +0200

    persist whether the zone is in maintenance mode

--- src/zone/state/mod.rs
+++ src/zone/state/mod.rs
@@ -85,6 +85,7 @@ impl Spec {
         match self {
             Self::V1(v1::Spec {
                 policy,
+                maintenance_mode,
                 instances,
                 source,
                 min_expiration,
@@ -138,6 +139,7 @@ impl Spec {
                     loader,
                     history,
                     persistence,
+                    maintenance_mode,
                     ..Default::default()
                 })
             }
