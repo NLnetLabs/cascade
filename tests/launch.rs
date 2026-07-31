b29b7ed36a84bf033b0f4249ffb0f261e2f7878a
arya dradjica <arya@nlnetlabs.nl>
2026-07-31 16:25:30 +0200

    [tests/launch] Test adding a policy

--- tests/launch.rs
+++ tests/launch.rs
@@ -8,5 +8,15 @@ use cascade_tests::process;
 #[test]
 fn launch() {
     let daemon = process::DaemonBuilder::new().build();
-    println!("{daemon:?}");
+
+    // Set up a simple policy.
+    let policy = cascade_policy_file::v1::Spec::default();
+    let policy = cascade_policy_file::VersionedSpec::V1(policy);
+    let policy = toml::to_string(&policy).unwrap();
+    let path = daemon.filesystem.policies.join("simple.toml");
+    std::fs::write(path, policy).unwrap();
+    println!("reload policies: {:?}", daemon.client.reload_policies());
+
+    println!("zone names: {:?}", daemon.client.zone_names());
+    println!("policy names: {:?}", daemon.client.policy_names());
 }
