4f9e7913f231b012520d619c6394b83e49f970f7
arya dradjica <arya@nlnetlabs.nl>
2026-08-14 16:08:28 +0200

    Make `loader::zonefile::load()` unit-testable

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -134,7 +134,7 @@ async fn refresh(
             let metrics = metrics.clone();
             let result;
             (builder, result) = tokio::task::spawn_blocking(move || {
-                let result = zonefile::load(&zone, &path, &mut builder, &metrics);
+                let result = zonefile::load(&zone.name, &path, &mut builder, &metrics);
                 (builder, result)
             })
             .await
