4e3e03663cd399d4a5a37728ee71c0ce0c15694f
arya dradjica <arya@nlnetlabs.nl>
2026-08-18 09:09:24 +0200

    Port the `remove-zone` test

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -2,10 +2,14 @@
 
 use std::sync::Arc;
 
+use camino::Utf8Path;
+use cascade_api as api;
+use domain::base::{Rtype, iana::OptRcode};
 use tokio::sync::Semaphore;
 use tracing_subscriber::EnvFilter;
 
 mod infra;
+use infra::ports;
 
 fn main() {
     tracing_subscriber::fmt()
@@ -19,6 +23,8 @@ fn main() {
         )
         .init();
 
+    let _ = infra::CURRENT_CONFIG.set(infra::TestConfig::default());
+
     let runtime = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
         .build()
@@ -27,13 +33,13 @@ fn main() {
     runtime.block_on(async {
         let docker = Arc::new(bollard::Docker::connect_with_defaults().unwrap());
 
-        let image = infra::ImageBuilder::new(docker).build().await;
+        let image = Arc::new(infra::ImageBuilder::new(docker).build().await);
 
-        let container = infra::ContainerBuilder::new(&image).build().await;
+        let mut tests = tokio::task::JoinSet::new();
 
-        let cascade = infra::Cascade::start(&container).await;
+        tests.spawn(remove_zone(image.clone()));
 
-        tracing::info!("Policy names: {:?}", cascade.policy_names().await);
+        tests.join_all().await;
     });
 
     runtime.block_on(async {
@@ -42,4 +48,70 @@ fn main() {
             .await
             .unwrap();
     });
+}
+
+#[tracing::instrument(level = "info", skip_all, fields(step))]
+async fn remove_zone(image: Arc<infra::Image>) {
+    let container = infra::ContainerBuilder::new(&image).build().await;
+    let cascade = Arc::new(infra::Cascade::start(&container).await);
+
+    tracing::info!("Add a zone");
+    let zone_name: api::ZoneName = "example.test".parse().unwrap();
+    cascade
+        .add_zone(api::ZoneAdd {
+            name: zone_name.clone(),
+            source: api::ZoneSource::Zonefile {
+                path: Utf8Path::new("/test/primary/example.test.zone").into(),
+            },
+            policy: "default".into(),
+            key_imports: vec![],
+        })
+        .await
+        .unwrap();
+
+    tracing::info!("Cascade should list the zone");
+    infra::poll(
+        || cascade.clone(),
+        async |c| c.zone_names().await,
+        |names| names.contains(&zone_name),
+    )
+    .await;
+
+    tracing::info!("Check zone status");
+    let _ = infra::poll(
+        || cascade.clone(),
+        async |c| c.zone_status("example.test").await,
+        |status| status.as_ref().is_ok_and(|s| s.last_published.is_some()),
+    )
+    .await;
+
+    tracing::info!("Querying the zone should succeed");
+    let res = container
+        .dns_query(ports::PUBLICATION, "example.test", Rtype::SOA, None)
+        .await;
+    assert!(res.as_ref().is_ok_and(|r| r.no_error()), "{res:?}");
+
+    tracing::info!("Remove the zone");
+    cascade.remove_zone("example.test").await.unwrap();
+
+    tracing::info!("Cascade should no longer list the zone");
+    infra::poll(
+        || cascade.clone(),
+        async |c| c.zone_names().await,
+        |names| !names.contains(&zone_name),
+    )
+    .await;
+
+    tracing::info!("Querying the zone should now fail");
+    let res = container
+        .dns_query(ports::PUBLICATION, "example.test", Rtype::SOA, None)
+        .await;
+    assert!(
+        res.as_ref()
+            .is_ok_and(|r| r.opt_rcode() == OptRcode::REFUSED),
+        "{res:?}"
+    );
+
+    tracing::info!("Check that Cascade is still running");
+    assert!(cascade.health().await.unwrap().healthy);
 }
